9bb8dcd09dbd3e6f8413f31fb3f6fd50f32e160b
John <40263018+johndejohn@users.noreply.github.com>
2025-09-27 19:09:55 +0100

    Update sidebar-history.tsx

--- components/sidebar-history.tsx
+++ components/sidebar-history.tsx
@@ -194,7 +194,7 @@ export function SidebarHistory({ user }: { user: User | undefined }) {
       <SidebarGroup>
         <SidebarGroupContent>
           <div className="flex w-full flex-row items-center justify-center gap-2 px-2 text-sm text-zinc-500">
-            Pwered by Mayday </n>
+            Pwered by Mayday <br/>
             Your conversations will appear here once you start chatting!
           </div>
         </SidebarGroupContent>
