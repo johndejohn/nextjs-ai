8352d68e810437a64d9efb6534a9590e7a66efcd
John <40263018+johndejohn@users.noreply.github.com>
2025-09-27 22:43:59 +0100

    Update sidebar-history.tsx

--- components/sidebar-history.tsx
+++ components/sidebar-history.tsx
@@ -194,7 +194,7 @@ export function SidebarHistory({ user }: { user: User | undefined }) {
       <SidebarGroup>
         <SidebarGroupContent>
           <div className="flex w-full flex-row items-center justify-center gap-2 px-2 text-sm text-zinc-500">
-            Powered by <a href='https://myday.id'> Mayday</a> <br />
+            <br>Powered by <a href='https://mayday.id'> Mayday</a> <br />
             <br/>
             Your conversations will appear here once you start chatting!
           </div>
